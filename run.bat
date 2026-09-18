cdb5be8fe2c0c9a89bddc77d6e5cfa89b0d94b4f
Armando Soriano <edwinacostasoriano@gmail.com>
2026-09-17 21:49:24 -0500

    Clean
    Clean project of junk files

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Qt\6.6.3\mingw_64\bin;C:\Users\User\AppData\Local\Microsoft\WinGet\Packages\BrechtSanders.WinLibs.POSIX.MSVCRT_Microsoft.Winget.Source_8wekyb3d8bbwe\mingw64\bin;%PATH%
-set QT_QUICK_BACKEND=software
-cd /d "%~dp0build"
-start "" ChecklistApp.exe
